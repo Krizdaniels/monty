f620183be5048c8195ec945085b74f938afbd861
Krizdaniels <dannychris2@gmail.com>
2023-11-16 11:39:47 -0800

    0x19 Stacks

---
+++ stck.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack - A function that prints the top
+ * @head: The stack head
+ * @counter: The line_number
+ * Return: Nothing
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
